d6bf93c776623355121964b0e24416d84830c22d
MartinKuehner <martin.kuehner@delta-ev.de>
2023-03-23 11:39:14 +0100

    Johnnys Scheiß aufgeräumt

--- lib/home.dart
+++ lib/home.dart
@@ -53,6 +53,25 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  int? _score;
+  double _progress = 0;
+
+  double _calcProgress() {
+    return _score! % 10 / 10;
+  }
+
+  void _initializeScore() {
+    _score =
+        0; /*Placeholder use the useres current score as initilization
+    Get the score from the database for the user account
+    */
+  }
+
+  void changeScore(int delta) {
+    _score = delta + _score!;
+    //Write Score back to user account
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -65,7 +84,50 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Spacer(
+                  flex: 1,
+                ),
+                IconButton(
+                  icon: const Icon(Icons.account_circle),
+                  iconSize: 40,
+                  onPressed: () {},
+                ),
+                const Spacer(
+                  flex: 2,
+                ),
+                Flexible(
+                  flex: 8,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    child: LinearProgressIndicator(
+                      backgroundColor: AppColors.white,
+                      valueColor:
+                          AlwaysStoppedAnimation<Color>(AppColors.accent),
+                      value: _progress,
+                      minHeight: 20,
+                    ),
+                  ),
+                ),
+                const Spacer(
+                  flex: 2,
+                ),
+                RichText(
+                  text: TextSpan(
+                      text: _score.toString(), style: AppTextStyles.scoreStyle),
+                ),
+                const Spacer(
+                  flex: 1,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it

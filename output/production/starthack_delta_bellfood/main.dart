f92f8c98108c32b01920db01cee0b32b47fa9790
Jonathan Kowa <jonathan@kowa.io>
2023-03-23 09:18:35 +0100

    add initial firebase configuration

--- output/production/starthack_delta_bellfood/main.dart
+++ output/production/starthack_delta_bellfood/main.dart
@@ -1,24 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'firebase_options.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
   runApp(const MyApp());
-
-  FirebaseAuth.instance
-      .authStateChanges()
-      .listen((User? user) {
-    if (user == null) {
-      print('User is currently signed out!');
-    } else {
-      print('User is signed in!');
-    }
-  });
 }
 
 class MyApp extends StatelessWidget {
@@ -27,8 +10,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -43,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'BellFood Sustainability'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
